06e2a32159d297c1e6f838bd99dcf69b5880d989
Hanson Yang <hansonyang73@gmail.com>
2025-12-14 15:21:04 -0500

    small change

---
+++ login.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+
+    $("#registerButton").click(() => {
+        window.location.href = "register.html";
+    })
+
+    $("#loginButton").click(() => {
+        loginInfo = {email: $("#email").val(), password: $("#password").val()}
+
+        $.ajax({
+		    method: "POST",
+		    url: "https://reqres.in/api/login",
+            contentType: "application/json",
+		    headers: {"x-api-key": "reqres_f1ac177d9f6748d1924e2bdb8a2f312c"},
+            data: JSON.stringify(loginInfo),
+            success: (r) => {console.log(r)},
+            error: (xhr) => {console.log(xhr.responseText)} 
+		});
+        
+        // fetch('https://reqres.in/api/users', {
+        // headers: { 'x-api-key': 'reqres_f1ac177d9f6748d1924e2bdb8a2f312c' }
+        // })
+        // .then(res => res.json())
+        // .then(console.log)
+        // .catch(console.error)
+    })
+})
