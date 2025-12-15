ecca946773e30b5e7e34a7fc50fbfb5b6dc99f4c
OliverDAvino <davinooliver@icloud.com>
2025-12-15 16:38:36 -0500

    cart button error

--- header.js
+++ header.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
     fetch("header.html").then(r => r.text())
                         .then(html => $("#header").html(html));
+
+  
+    $("#cartButton").click(() => {
+        window.location.href = "cart.html";
+    })
 })
+
+
