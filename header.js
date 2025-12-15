cce0682fe3c704beb361d82175ae4eacafe94b71
Hanson Yang <hansonyang73@gmail.com>
2025-12-15 16:43:38 -0500

    fix card button

--- header.js
+++ header.js
@@ -1,11 +1,12 @@
 $(document).ready(() => {
     fetch("header.html").then(r => r.text())
-                        .then(html => $("#header").html(html));
-
-  
-    $("#cartButton").click(() => {
-        window.location.href = "cart.html";
-    })
+                        .then(html => $("#header").html(html))
+                        .then(() => {
+                            $("#cartButton").click(() => {
+                                console.log(1);
+                                window.location.href = "cart.html";
+                            })
+                        });
 })
 
 
