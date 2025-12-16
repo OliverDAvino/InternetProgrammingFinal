0ca3de379e4b3bd1d7c3a9acd620155675559918
OliverDAvino <davinooliver@icloud.com>
2025-12-15 22:06:45 -0500

    Cart worked on and stuff

--- header.js
+++ header.js
@@ -1,12 +1,20 @@
-$(document).ready(() => {
-    fetch("header.html").then(r => r.text())
-                        .then(html => $("#header").html(html))
-                        .then(() => {
-                            $("#cartButton").click(() => {
-                                console.log(1);
-                                window.location.href = "cart.html";
-                            })
-                        });
-})
+$(document).ready(async function () {
+  await loadHeader();
+  setupHeaderButtons();
+  // later: setupLiveSearch();
+});
 
+async function loadHeader() {
+  const html = await fetch("header.html").then(r => r.text());
+  $("#header").html(html);
+}
 
+function setupHeaderButtons() {
+  $("#cartButton").click(() => {
+    window.location.href = "cart.html";
+  });
+
+  $("#signOutButton").click(() => {
+    window.location.href = "login.html";
+  });
+}
