e14743c8d71fe5e7a3cf26661fabe4fb7e305f5c
Hanson Yang <hansonyang73@gmail.com>
2025-12-19 13:55:43 -0500

    a

--- Checkout.js
+++ Checkout.js
@@ -1,8 +1,14 @@
 $(document).ready(() => {
+    $("#checkoutForm").submit((e) => {
+        e.preventDefault();
+        if (validInput()){
+            window.location.href = "orderConfirmation.html";
+        }
+    });
+
     $("#total").html("Total (Tax included): " + parseInt(getCookieValue("total")).toFixed(2) + "$");
 
     let cart = Object.values(JSON.parse(getCookieValue("cart")));
-    console.log(cart);
     cart.forEach((p) => {
         $("#summary").append(`<div class="item">${p.name} (${p.price}$) Qty: ${p.quantity}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${(parseFloat(p.price)*parseInt(p.quantity)).toFixed(2)}$</div>`);
     });
@@ -19,6 +25,32 @@ function getCookieValue(cookieName){
     return 0;
 }
 
-function checkInput(){
-    
+function validInput(){
+    // text inputs
+    let inputs = [
+        document.getElementById("fn"),
+        document.getElementById("ln"),
+        document.getElementById("email"),
+        document.getElementById("phone"),
+        document.getElementById("shipping")
+    ];
+
+    // check empty text inputs
+    for (let input of inputs) {
+        if (input.value.trim() == "") {
+            alert("Please fill in all fields.");
+            input.focus();
+            return false;
+        }
+    }
+
+    // check radio buttons
+    let deliveryChecked = document.querySelector('input[name="delivery"]:checked');
+
+    if (!deliveryChecked) {
+        alert("Please select a delivery option.");
+        return false;
+    }
+
+    return true;
 }
