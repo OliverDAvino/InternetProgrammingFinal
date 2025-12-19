ade70acf9b3add77b1aa0824f0c55cf891d12e86
Hanson Yang <hansonyang73@gmail.com>
2025-12-19 14:33:30 -0500

    a

---
+++ orderConfirmation.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+    $("#orderNumber").html("Order number: " + (Math.floor(Math.random()*8999999999999 + 1000000000000)));
+
+    showOrder();
+});
+
+function showOrder(){
+    $("#total").html("Total (Tax included): " + parseInt(getCookieValue("total")).toFixed(2) + "$");
+
+
+    let cart = Object.values(JSON.parse(getCookieValue("cart")));
+    cart.forEach((p) => {
+        $("#summary").append(`<div class="item">${p.name} (${p.price}$) Qty: ${p.quantity}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${(parseFloat(p.price)*parseInt(p.quantity)).toFixed(2)}$</div>`);
+    });
+}
+
+function getCookieValue(cookieName){
+    var cookies = document.cookie.split(";");
+    for (var cookie of cookies){
+        var [name, value] = cookie.split("=");
+        if (name.trim() == cookieName){
+            return value;
+        }
+    }
+    return 0;
+}
